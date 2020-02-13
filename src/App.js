c44aa6d2eacd8dd4b4a5dbe9634b80c97f4ff70f
AidenBow <AidenBow@users.noreply.github.com>
2020-02-13 14:47:24 -0800

    updated header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          movie party staging
+          movie party production
         </p>
       </header>
     </div>
