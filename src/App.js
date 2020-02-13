bbab373640b1f09d8c5baa117990c1bb17912a42
AidenBow <AidenBow@users.noreply.github.com>
2020-02-13 14:24:38 -0800

    changed header in index and app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          movie party
+          movie party staging
         </p>
       </header>
     </div>
