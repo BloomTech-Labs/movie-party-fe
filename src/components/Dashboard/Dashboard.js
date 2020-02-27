a1ab953cf2534258592dfb1cb95abf77c4df85c2
AidenBow <AidenBow@users.noreply.github.com>
2020-02-26 20:37:38 -0800

    added title filler data

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,15 +3,30 @@ import "./dashboard.css";
 import EventCard from "./EventCard";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [
+        {title: "scooby doo scary monster"},
+        {title: "scooby doo scary monster TWO!!!"},
+        {title: "scooby doo scary monster 3"},
+        {title: "scooby doo scary monster FOUR???"},
+        {title: "scooby doo scary monster fiv--"},
+        {title: "scooby doo scary monster & six!"},
+      ],
+    }
+  }
   render() {
+    console.log(this.state.events)
     return (
-      <div class="dashboardEventContainer">
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
+      <div>
+        <h1>test</h1>
+        <div className="dashboardEventContainer">
+          { this.state.events.map(event => {
+            return <EventCard title={event.title}/>
+          })}
+          
+        </div>
       </div>
     )
   }
--- src/components/Dashboard/EventCard.js
+++ src/components/Dashboard/EventCard.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 
 class EventCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      events: [],
-    }
-  }
 
   render() {
+    console.log("in eventcard")
     return (
-      <div class="eventCardContainer">
-        <div class="eventCardImg"></div>
-        <h5>Title</h5>
+      <div className="eventCardContainer">
+        <div className="eventCardImg"></div>
+        <h5>{this.props.title}</h5>
         <p>date</p>
         <p>time</p>
       </div>
