bcae6b51ca0886e72ab67f28e0c7b3c22323f467
AidenBow <AidenBow@users.noreply.github.com>
2020-03-16 19:42:46 -0700

    example modal

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./dashboard.css";
 import EventCard from "./EventCard";
+import AddPartyModal from "./AddPartyModal"
 
 class Dashboard extends Component {
   constructor(props) {
@@ -23,9 +24,9 @@ class Dashboard extends Component {
         <div className="dashboardHeader">
           <h3>Upcoming Parties</h3>
           <h3>Filter</h3>
-          <h3>+</h3>
+          <AddPartyModal />
         </div>
-
+      
         <div className="dashboardEventContainer">
           { this.state.events.map(event => {
             return <EventCard key={event.id} title={event.title} date={event.date} time={event.time}/>
