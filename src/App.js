99741f1e0d10a45fcb8fb6f9291b4ac4a390a8dd
Antony Nyagah <antonynyagah@Antonys-MBP.attlocal.net>
2020-03-18 23:55:41 -0400

    Nav and header,needs styling done to match UX design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import axios from 'axios'
 import Route from 'react-router-dom'
 import './App.css';
+import navigation from './components/navigation'
 
 /*function App() {
   return (
@@ -15,7 +15,7 @@ import './App.css';
       </header>
     </div>
   );
-}*/
+}
 
 class App extends Component {
   componentDidMount() {
@@ -30,10 +30,39 @@ class App extends Component {
       <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <navigation></navigation>
     </div>
     );
   }
+}*/
+import {Link} from 'react-router-dom';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="demo-big-content">
+    <Layout>
+        <Header className ="header-color" scroll>
+            <h1>MOVIE PARTY</h1>
+        </Header>
+        <Drawer>
+            <Navigation className="navigation-color">
+                <Link exact path to="/"><h1>Upcoming Parties</h1></Link>
+                <Link to="/resume"><h1>Past Parties</h1></Link>
+                <Link to="/projects"><h1>Friends</h1></Link>
+                <Link to="/aboutme"><h1>Films</h1></Link>
+                <Link to="/contact"><h1>WatchList</h1></Link>
+                <Link to="/contact"><h1>Create New Party</h1></Link>
+            </Navigation>
+        </Drawer>
+    </Layout>
+</div>
+     
+    </div>
+  );
 }
 
-
 export default App;
+
+
