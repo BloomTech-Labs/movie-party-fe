41434de75701aecb3018a8e3239f02d54ff24c17
raberin <rmvaberin@gmail.com>
2020-04-07 18:52:01 -0700

    Added Register component

---
+++ src/components/authentication/Login/Login.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import authAxios from "../../../utils/authAxios";
+import { Link } from "react-router-dom";
+import "../authStyles.css";
+
+const Login = props => {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const userInput = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const userLogin = e => {
+    e.preventDefault();
+    authAxios()
+      .post(`https://archlife-be.herokuapp.com/api/auth/login`, user)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.token);
+        props.history.push("/demo");
+      })
+      .catch(err => console.log(err, "for sure error"));
+  };
+
+  return (
+    <div className="authContainer">
+      <div className="inner-container">
+        <div className="box">
+          <h1>Login</h1>
+          <form onSubmit={userLogin}>
+            <label>email</label> <br />
+            <input
+              name="email"
+              placeholder="email"
+              type="email"
+              onChange={userInput}
+              value={user.email}
+              autoComplete="off"
+            />
+            <br />
+            <label>password</label> <br />
+            <input
+              name="password"
+              placeholder="password"
+              type="password"
+              onChange={userInput}
+              value={user.password}
+              autoComplete="off"
+            />
+            <br />
+            <button>Login</button>
+            <p>
+              Not a member?{" "}
+              <Link to="/register">
+                <span>Sign Up</span>
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/authentication/Register/Register.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "../authStyles.css";
+
+const Register = props => {
+  const [user, setUser] = useState({
+    first_name:'',
+    last_name:'',
+    email: "",
+    password: ""
+  });
+
+  const userInput = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const userRegistration = e => {
+    e.preventDefault();
+    axios
+      .post(`https://movie-party-be-stag.herokuapp.com/api/register`, user)
+      .then(res => {
+        console.log(res);
+        props.history.push("/login");
+      })
+      .catch(err => console.log(err, "for sure error"));
+  };
+
+  return (
+    <div className="authContainer">
+      <div className="inner-container">
+        <div className="box">
+          <h1>Register</h1>
+          <form onSubmit={userRegistration}>
+            <label>First Name</label> <br />
+            <input
+              name="first_name"
+              placeholder="First Name"
+              type="text"
+              onChange={userInput}
+              value={user.first_name}
+              autoComplete="off"
+            />
+            <br />
+            <label>Last Name</label> <br />
+            <input
+              name="last_name"
+              placeholder="Last Name"
+              type="text"
+              onChange={userInput}
+              value={user.last_name}
+              autoComplete="off"
+            />
+            <br />
+            <label>email</label> <br />
+            <input
+              name="email"
+              placeholder="email"
+              type="email"
+              onChange={userInput}
+              value={user.email}
+              autoComplete="off"
+            />
+            <br />
+            <label>password</label> <br />
+            <input
+              name="password"
+              placeholder="password"
+              type="password"
+              onChange={userInput}
+              value={user.password}
+              autoComplete="off"
+            />
+            <br />
+            <button>Register</button>
+            <p>
+              NOTE: Upon clicking Register, please wait patiently <br /> for a
+              moment or two...
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
